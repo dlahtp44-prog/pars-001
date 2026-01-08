ae52e9bbf538b22611fdb22aa31ba369e8161cd9
dlahtp44-prog <dlahtp44@gmail.com>
2026-01-08 20:21:05 +0900

    Update index_page.py

--- app/pages/index_page.py
+++ app/pages/index_page.py
@@ -5,7 +5,6 @@ from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="app/templates")
 
 router = APIRouter(
-    prefix="/",
     tags=["Page"],
     include_in_schema=False
 )
