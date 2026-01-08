e6197ce58a09846861c917355d7348eb21afcf81
dlahtp44-prog <dlahtp44@gmail.com>
2026-01-08 19:01:13 +0900

    Update history_page.py

--- app/pages/history_page.py
+++ app/pages/history_page.py
@@ -1,12 +1,30 @@
-
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-templates = Jinja2Templates(directory="app/templates")
-router = APIRRouter(prefix="/history", tags=["Page"], include_in_schema=False)
+# =====================================================
+# TEMPLATES
+# =====================================================
 
+templates = Jinja2Templates(directory="app/templates")
+
+# =====================================================
+# ROUTER
+# =====================================================
+
+router = APIRouter(
+    prefix="/page/history",
+    tags=["Page"],
+    include_in_schema=False
+)
+
+# =====================================================
+# PAGE
+# =====================================================
 
 @router.get("/", response_class=HTMLResponse)
 def history_page(request: Request):
-    return templates.TemplateResponse("history.html", {"request": request})
+    return templates.TemplateResponse(
+        "history.html",
+        {"request": request}
+    )
