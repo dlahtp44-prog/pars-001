7e0517f62a0ff00c43df864e10b84ffbb41d1659
dlahtp44-prog <dlahtp44@gmail.com>
2026-01-09 12:08:56 +0900

    Update excel_labels.py

--- app/pages/excel_labels.py
+++ app/pages/excel_labels.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from app.core.paths import TEMPLATES_DIR
 
 router = APIRouter(
     prefix="/page/labels",
     tags=["라벨"]
 )
 
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
 
 
 @router.get("", response_class=HTMLResponse)
